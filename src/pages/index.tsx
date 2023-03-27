9b6bc802795d4e323142065ffe6429d3d145a7ab
Leenus Darnal <leenus.darnal@gmail.com>
2023-03-27 23:52:51 +0530

    Initial Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className='text-9xl text-red-700 flex justify-center items-center '>
+        Under Construction...
+      </h1>
+    </>
+  )
+}
