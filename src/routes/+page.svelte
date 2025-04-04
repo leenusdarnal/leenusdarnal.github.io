989aada2a10861c5cd386832112f1fa3e7c5e1df
leenusdarnal <leenus.darnal.com>
2025-04-04 09:31:42 +0000

    made changes for static pages

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<main class="container p-1">
+<main class="container">
 	<div class="flex items-center justify-center">
-		<h1 class="text-9xl text-red-800">Under Construction</h1>
+		<h1 class="text-9xl text-red-900">Under Construction</h1>
 	</div>
 </main>
