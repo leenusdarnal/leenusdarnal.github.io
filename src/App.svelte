0d8b0c39eba8d27dfc0f49742955d3dff60fb207
leenusdarnal <leenus.darnal.com>
2025-04-04 09:03:24 +0000

    project moved to SvelteKit

--- src/App.svelte
+++
@@ -1,1 +0,0 @@
-<h1 class="text-9xl flex justify-center text-red-600">Under Construction</h1>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<main class="container p-1">
+	<div class="flex items-center justify-center">
+		<h1 class="text-9xl text-red-800">Under Construction</h1>
+	</div>
+</main>
